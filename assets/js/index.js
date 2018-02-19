8eaa64db8ddcd7aabeda1e634019d141dbeaf56e
dmann10 <dylan.mann10@gmail.com>
2018-02-19 02:07:45 -0500

    Created the Services and Contact Us section

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,51 @@
+// Toggle services description onHover
+$(document).ready(function() {
+
+(function() {
+    // Slide Down
+    $('.serviceInfo').mouseenter(function() {
+        var description = $(this).children('.serviceInfoText');
+        
+        $(this).data('timeout', setTimeout(function () {
+            description.slideDown("slow"); 
+           }, 500))
+        .mouseleave(function() {
+            clearTimeout($(this).data('timeout'));
+        });
+    });
+
+    // Slide Up
+    $('.serviceInfo').mouseleave(function() {
+        var description = $(this).children('.serviceInfoText');
+        
+        if(description.not(":hidden"))
+            description.slideUp("slow");
+    });
+
+})();
+});
+
+// Navigation functions
+function scrollToHome() {
+    $('html, body').animate({
+        scrollTop: $(".navbar").offset().top
+    }, 1000);
+}
+
 function scrollToAbout() {
     $('html, body').animate({
         scrollTop: $(".aboutUsSection").offset().top
+    }, 1000);
+}
+
+function scrollToServices() {
+    $('html, body').animate({
+        scrollTop: $(".serviceSection").offset().top
+    }, 1000);
+}
+
+function scrollToContactUs() {
+    $('html, body').animate({
+        scrollTop: $("#contactUsSection").offset().top
     }, 1000);
 }
